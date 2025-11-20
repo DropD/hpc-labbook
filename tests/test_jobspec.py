d716e830b0433510e2d72c32e76236833d0a518e
Rico Häuselmann <rico.haeuselmann@gmail.com>
2025-11-20 17:34:08 +0100

    fix jobspec tests

--- tests/test_jobspec.py
+++ tests/test_jobspec.py
@@ -2,7 +2,9 @@
 
 from __future__ import annotations
 
+import pathlib
 import typing
+from typing import Callable
 
 import pytest
 
@@ -13,23 +15,27 @@ if typing.TYPE_CHECKING:
     from aiida.orm import Code, InstalledCode
 
 
-def test_minimal_spec(aiida_code_installed: InstalledCode) -> None:
+def test_minimal_spec(aiida_code_installed: Callable[..., InstalledCode]) -> None:
     """Make sure no fields are required that shouldn't be."""
     spec = jobspec.Generic(
-        code=aiida_code_installed.label,
+        code=(code := aiida_code_installed()).label,
         workdir=data.TargetDir("root"),
         label="testjob",
         description="Minimal GenericJobCalc spec",
         queue="default",
     )
-    assert spec.load_code() == aiida_code_installed
-    assert spec.load_computer() == aiida_code_installed.computer
+    assert spec.load_code() == code
+    assert spec.load_computer() == code.computer
     _ = spec.to_builder()  # make sure no errors thrown
 
 
-def test_missing_comp(aiida_code: typing.Callable[..., Code]) -> None:
+def test_missing_comp(aiida_code: Callable[..., Code]) -> None:
     """Make sure that missing computer raises error."""
-    code = aiida_code("core.code.portable")
+    code = aiida_code(
+        "core.code.portable",
+        filepath_executable="dummy.sh",
+        filepath_files=pathlib.Path(__file__).parent / "data",
+    )
     spec = jobspec.Generic(
         code=code.label,
         workdir=data.TargetDir("root"),
